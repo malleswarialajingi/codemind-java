7bef02da8233f33f01746f6c27c3b4f540993733
Naga Malleswari <147472888+malleswarialajingi@users.noreply.github.com>
2024-08-05 14:32:01 +0530

    import java.util.*; public class Maxi{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int m = sc.nextInt();         System.out.println(5*n + 7*m);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Maxi{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(5*n + 7*m);
+    }
+}
